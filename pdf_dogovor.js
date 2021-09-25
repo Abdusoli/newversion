46ffbdcbdf93df6d6179bf77d934cee12cdb5334
abdus <abdusoli26@gmail.com>
2021-09-25 17:16:14 +0500

    first commit

---
+++ pdf_dogovor.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("invoice");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 0,
+                filename: 'myfile.pdf',
+                image: { type: 'jpeg', quality: 1 },
+                html2canvas: { scale: 4 },
+                jsPDF: { unit: 'in', format: 'A4', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        })
+}
+
+
+
